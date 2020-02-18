b6bfbb4804b8d754d6743ed56088e22a5bf9f34d
adizmirli <azmirli@fieldintech.com>
2020-02-18 13:12:13 +0000

    init

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import time
+import mysql
 import pandas as pd
+from app_conf import *
 
-data = pd.DataFrame()
 
+connection = mysql.connector.connect(**conn)
+cursor = connection.cursor(buffered=True)
+
+mysql_example = f"select *" \
+                f"from device_works"
+
+cursor.execute(mysql_example)
+
+if cursor.rowcount > 0:
+    df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
+cursor.close()
+connection.close()
